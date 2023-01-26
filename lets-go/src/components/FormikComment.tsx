addb2d4fe02d98e0baeb84087c97685be3931bbb
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2023-01-26 17:00:24 -0600

    Update FormikComment.tsx

--- lets-go/src/components/FormikComment.tsx
+++ lets-go/src/components/FormikComment.tsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik,Field,Form } from "formik";
 
 const CommentForm = () => {
   <div>
     <Formik
       initialValues={{
-        name: "",
-        comment: "",
+        name: '',
+        comment: '',
       }}
       onSubmit={async (values) => {
-        await new Promise((r) => setTimeout(r, 300));
+        await new Promise((r) => setTimeout(r, 500));
         alert(JSON.stringify(values,null,2));
       }}
     >
 
       <Form>
-        
+        <label htmlFor="name">Name:</label>
+        <Field id="name" name="name" placeholder=""/>
+
+        <label htmlFor="comment">Comment:</label>
+        <Field></Field>
       </Form>
     </Formik>
   </div>;
